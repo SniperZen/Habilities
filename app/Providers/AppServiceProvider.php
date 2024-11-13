8dee9580bef26e663029b290730c62e93a79607f
deguzmanharold2@gmail.com <deguzmanharold2@gmail.com>
2024-11-13 13:02:00 +0800

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,10 +17,8 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot()
+    public function boot(): void
     {
-        Blade::component('components.patient-layout', 'patient-layout');
+        //
     }
-
-    
 }
